ea9d0db976c9154c6f57945e8183eeec4af826a7
anukultyagi <anukultyagi321@gmail.com>
2024-05-13 23:43:01 +0530

    changed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Header = () => {
     return (
         <header className='container max-w-6xl mx-auto py-10 '>
-            <div className='flex flex-col gap-10 m-10 items-center text-center'>
-                <p className='text-md '>Browse through a curated selection of Chrome extensions handpicked for productivity, creativity, and convenience. Get ready to supercharge your browsing experience! </p>
-                <p className='text-6xl '>Transform Your productivity with Chrome Vault. </p>
+            <div className='flex flex-col gap-5 m-10 items-center text-center'>
+                <p className='text-md w-2/3'>Browse through a curated selection of Chrome extensions handpicked for productivity, creativity, and convenience. Get ready to supercharge your browsing experience! </p>
+                <p className='text-6xl mb-5'>Transform Your productivity with Chrome Vault. </p>
                 <button className='w-fit mb-2 bg-black font-light text-sm text-white px-6 py-3 rounded-full tracking-widest'>Instructions</button>
             </div>
         </header>
