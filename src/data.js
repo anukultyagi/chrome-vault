8fe23738e9cb7e1303bcac7d713f05f2a69d0607
anukultyagi <anukultyagi321@gmail.com>
2024-06-20 13:48:45 +0530

    changed card  order

--- src/data.js
+++ src/data.js
@@ -5,21 +5,21 @@ const { dadjoke, loremIpsum, lorempicsum, pesticide, colorPicker, adBlocker } = 
 export const cardData = [
     {
         id: 1,
-        imgSrc: dadjoke,
-        title: 'Dad Joke Extension 😂',
-        desc: 'This extension provides a daily dose of hilarious dad jokes. Click on the extension to get a good laugh every time!',
+        imgSrc: colorPicker,
+        title: 'Color Picker',
+        desc: 'Easily select and save any color from your screen using our Color Picker. Perfect for designers and developers needing quick color references.',
     },
     {
         id: 2,
-        imgSrc: lorempicsum,
-        title: 'Lorem Picsum Extension 🖼',
-        desc: 'Browse high-quality images from Lorem Picsum directly in your browser. Perfect for designers and developers looking for inspiration and beautiful visuals!',
-    },
-    {
-        id: 3,
         imgSrc: pesticide,
         title: 'Pesticide (CSS Debugger)',
         desc: 'Pesticide outlines every element on your webpage, helping you quickly identify and debug CSS layout issues with ease and efficiency.',
+    },
+    {
+        id: 3,
+        imgSrc: dadjoke,
+        title: 'Dad Joke Extension 😂',
+        desc: 'This extension provides a daily dose of hilarious dad jokes. Click on the extension to get a good laugh every time!',
     },
     {
         id: 4,
@@ -29,9 +29,9 @@ export const cardData = [
     },
     {
         id: 5,
-        imgSrc: colorPicker,
-        title: 'Color Picker',
-        desc: 'Easily select and save any color from your screen using our Color Picker extension. Perfect for designers and developers needing quick color references.',
+        imgSrc: lorempicsum,
+        title: 'Lorem Picsum Extension 🖼',
+        desc: 'Browse high-quality images from Lorem Picsum directly in your browser. Perfect for designers and developers looking for inspiration and beautiful visuals!',
     },
     {
         id: 6,
